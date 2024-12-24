899d9569fc7a245564302ef4391a510a9eff75ac
rmaliditia <aliditia123@gmail.com>
2024-12-24 10:31:40 +0700

    Menyelesaikan 80% Portofolio

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
+// Navigation Top
 document.addEventListener("DOMContentLoaded", function () {
   const navbarMenu = document.getElementById("navbarMenu");
   const navLinks = navbarMenu.getElementsByTagName("a");
 
   for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("click", function () {
-      // Menghapus kelas 'active' dari semua a
       for (let j = 0; j < navLinks.length; j++) {
         navLinks[j].classList.remove("active");
       }
-      // Menambahkan kelas 'active' ke a yang diklik
+
       this.classList.add("active");
     });
   }
+});
+
+// Navigation Bottom
+
+const navLinks = document.querySelectorAll(".navbarMenu-bottom a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((link) => link.classList.remove("active"));
+
+    link.classList.add("active");
+  });
 });
 
 // Gsap
 // Default GSAP settings
 gsap.defaults({ ease: "none" });
 
-// Animasi teks "a UI/UX Designer and Frontend Developer"
+// Animasi teks
 const tl = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true });
 tl.to(".intro .intro1", {
   duration: 3.7,
@@ -27,18 +39,16 @@ tl.to(".intro .intro1", {
 });
 
 // Skills Start
-// Add this JavaScript for the toggle functionality
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtns = document.querySelectorAll(".toggleBtn");
 
   toggleBtns.forEach((btn) => {
     btn.addEventListener("click", function () {
-      // Remove active class from all buttons and sections
       toggleBtns.forEach((b) => b.classList.remove("active"));
       document.querySelector(".techstack").classList.remove("active");
       document.querySelector(".tools").classList.remove("active");
 
-      // Add active class to clicked button and corresponding section
       this.classList.add("active");
       document.querySelector("." + this.dataset.target).classList.add("active");
     });
